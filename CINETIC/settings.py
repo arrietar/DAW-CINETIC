05d2587db00d51837c8c2baf9db68f80d484b20e
Carlos Arroyo <carloscha82@hotmail.com>
2022-08-27 17:16:48 -0500

    Se renombra la base de datos SQLite

--- CINETIC/settings.py
+++ CINETIC/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'CINETIC.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db.cinetic',
     }
 }
 
