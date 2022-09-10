f5b23296439055eec75a66455f98a1c82adbc5c1
Carlos Arroyo <carloscha82@hotmail.com>
2022-09-10 16:54:52 -0500

    Se crea carpeta static para almacenar las imagenes

--- CINETIC/settings.py
+++ CINETIC/settings.py
@@ -128,7 +128,6 @@ STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = { # para habilitar la autenticacion
         'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.IsAuthenticated'],
